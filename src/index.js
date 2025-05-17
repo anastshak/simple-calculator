db79a9504f639a55688bacd33d258beea288a627
anastshak <nastyashakura@gmail.com>
2025-05-17 15:40:45 +0300

    feat: add root element and title

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
+'use strict';
+
 import './styles/style.css';
 
-// console.log('hello');
+const app = document.createElement('div');
+app.id = 'root';
+document.body.appendChild(app);
+
+const title = document.createElement('h1');
+title.className = 'title';
+title.textContent = 'Simple Calculator';
+app.appendChild(title);
+
+const calculator = document.createElement('main');
+calculator.className = 'calculator';
+app.appendChild(calculator);
